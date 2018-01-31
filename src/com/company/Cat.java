26d7a6d47ae72383db37e037e17246822f5b27cb
Victoria West <victoriawest811@gmail.com>
2018-01-31 16:22:11 -0500

    Cat stuff

---
+++ src/com/company/Cat.java
@@ -0,0 +1,79 @@
+package com.company;
+
+public class Cat {
+
+    private int legs;
+    private boolean fur;
+    private String color;
+    private boolean tail;
+    private String pattern;
+    private String eyeColor;
+
+    public Cat(int legs, boolean fur, String color, boolean tail, String pattern, String eyeColor) {
+        this.legs = legs;
+        this.fur = fur;
+        this.color = color;
+        this.tail = tail;
+        this.pattern = pattern;
+        this.eyeColor = eyeColor;
+    }
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public void setLegs(int legs) {
+        this.legs = legs;
+    }
+
+    public boolean isFur() {
+        return fur;
+    }
+
+    public void setFur(boolean fur) {
+        this.fur = fur;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isTail() {
+        return tail;
+    }
+
+    public void setTail(boolean tail) {
+        this.tail = tail;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String EyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public boolean getFur() {
+        return fur;
+    }
+
+    public boolean getTail() {
+        return tail;
+    }
+
+
+    }
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
 
 public class Main {
+public static class Main2 {
+
 
     public static void main(String[] args) {
-	// write your code here
+        Cat cat = new Cat(4, true, "black", false, "solid", "yellow");
+
+        System.out.println("Your cat has the following traits: ");
+        System.out.println("Amount of legs: " + cat.getLegs());
+        System.out.println("Presence of fur: " + cat.getFur());
+        System.out.println("Fur color: " + cat.getColor());
+        System.out.println("Presence of a tail: " + cat.getTail());
+        System.out.println("Pattern of fur: " + cat.getPattern());
+        System.out.println("Eye color: " + cat.getEyeColor());
+
+
     }
 }
+
+    public static void main(String[] args) {
+        Cat cat = new Cat(4, true, "orange", true, "tabby", "green");
+
+        System.out.println("Your cat has the following traits: ");
+        System.out.println("Amount of legs: " + cat.getLegs());
+        System.out.println("Presence of fur: " + cat.getFur());
+        System.out.println("Fur color: " + cat.getColor());
+        System.out.println("Presence of a tail: " + cat.getTail());
+        System.out.println("Pattern of fur: " + cat.getPattern());
+        System.out.println("Eye color: " + cat.getEyeColor());
+
+    }
+}
+
+
